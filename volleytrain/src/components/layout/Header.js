677fb1f34002b720cdeacf4d3a133ab2a0b5f1d8
TimmMoetz <timmlouis@googlemail.com>
2021-06-10 11:10:02 +0200

    Revert "delete logout button"
    This reverts commit 387b214ba11fefde3c3a48dba5409b173bbc3ba2.

--- volleytrain/src/components/layout/Header.js
+++ volleytrain/src/components/layout/Header.js
@@ -15,6 +15,8 @@ import MailIcon from '@material-ui/icons/Mail';
 import ProfileDropDown from '../dialogs/ProfileDropDown'
 import Box from '@material-ui/core/Box';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { Button } from '@material-ui/core';
+import firebase from 'firebase'
 
 const drawerWidth = 240;
 
@@ -88,6 +90,10 @@ export default function Header(props) {
                 <ListItemIcon><SettingsIcon /></ListItemIcon>
                 </ListItem>
 
+                <ListItem>
+                  <Button onClick={()=>firebase.auth().signOut()}>Logout</Button>
+                </ListItem>
+                
             </Box>
             
             
