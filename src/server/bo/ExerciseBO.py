4690c84dd606c6763edd38a92035a7cb7a857394
TimmMoetz <timmlouis@googlemail.com>
2021-06-10 11:41:24 +0200

    Revert "revert ExerciseBO"
    This reverts commit dc403e862f00fb660368ccd34097feb61bb8b9b9.

---
+++ src/server/bo/ExerciseBO.py
@@ -0,0 +1,40 @@
+from server.bo.NamedBusinessObject import NamedBusinessObject
+
+
+class Exercise(NamedBusinessObject):
+    """Exercise Business Object
+    has following properties
+    tag
+    duration
+    """
+    def __init__(self):
+        super.__init__()
+        self._tag = None
+        self._duration = None
+
+    def setTag(self, tag):
+        self._tag = tag
+
+    def getTag(self):
+        return self._tag
+
+    def setDuration(self, duration):
+        self._duration = duration
+
+    def getDuration(self):
+        return self._duration
+
+    def __str__(self):
+        return "Exercise: {} {} {} {}".format(self._getID(), self._name, self._tag, self._duration)
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        ''' turn a python dict into a user object
+        '''
+
+        obj = Exercise()
+        obj.setId(dictionary["id"])  # part of the Business object mother class
+        obj.setName(dictionary["name"])
+        obj.setTag(dictionary["tag"])
+        obj.setDuration(dictionary["duration"])
+        return obj
