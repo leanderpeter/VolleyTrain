387b214ba11fefde3c3a48dba5409b173bbc3ba2
TimmMoetz <timmlouis@googlemail.com>
2021-06-10 11:01:47 +0200

    delete logout button

--- volleytrain/src/components/layout/Header.js
+++ volleytrain/src/components/layout/Header.js
@@ -15,8 +15,6 @@ import MailIcon from '@material-ui/icons/Mail';
 import ProfileDropDown from '../dialogs/ProfileDropDown'
 import Box from '@material-ui/core/Box';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { Button } from '@material-ui/core';
-import firebase from 'firebase'
 
 const drawerWidth = 240;
 
@@ -90,10 +88,6 @@ export default function Header(props) {
                 <ListItemIcon><SettingsIcon /></ListItemIcon>
                 </ListItem>
 
-                <ListItem>
-                  <Button onClick={()=>firebase.auth().signOut()}>Logout</Button>
-                </ListItem>
-                
             </Box>
             
             
